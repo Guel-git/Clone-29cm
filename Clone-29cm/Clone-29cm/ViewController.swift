80aaded899776eebd2282970d68aab6d3835a77f
Guel-git <eccyuna@gmail.com>
2023-08-22 15:53:37 +0900

    [ADD] 프로젝트 생성

---
+++ Clone-29cm/Clone-29cm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone-29cm
+//
+//  Created by 김유나 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Clone-29cm/Clone-29cmUITests/Clone_29cmUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Clone_29cmUITestsLaunchTests.swift
+//  Clone-29cmUITests
+//
+//  Created by 김유나 on 2023/08/22.
+//
+
+import XCTest
+
+final class Clone_29cmUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
